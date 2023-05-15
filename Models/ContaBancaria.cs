c58ad578a8c986107605b40ef10cfd9fe5211d7b
AlexSan1910 <alex.gsan11@gmail.com>
2023-05-15 17:28:50 -0300

    Conta Bancaria

---
+++ Models/ContaBancaria.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioBancario{
+    public class ContaBancaria{
+        // definindo propriedades
+
+        private int _numero;
+        public int Numero
+        {
+            get { return _numero; }
+            set { _numero = value; }
+        }
+
+        private string _titular;
+        public string Titular
+        {
+            get { return _titular; }
+            set { _titular = value; }
+        }
+
+        private decimal _saldo;
+        public decimal Saldo
+        {
+            get { return _saldo; }
+            set { _saldo = value; }
+        }
+
+        /*/ definindo construtores
+
+        public ContaBancaria(int _numero, string _titular, decimal _saldo)
+        {
+            this.Numero = _numero;
+            this.Titular = _titular;
+            this.Saldo = _saldo;
+        }*/
+
+        // Definindo metodos ou funções
+
+        public void Depositar(decimal deposito){
+             Saldo += deposito;
+        }
+
+        public void Sacar(decimal saque){
+
+            if (Saldo >= saque){
+                Saldo -= saque;
+            } else {
+                Console.WriteLine("Sinto Muito o seu Saldo é insuficiente!");
+            }
+            
+        }
+
+        public void Transferencia(ContaBancaria contaDestino, decimal valorTransferencia){
+
+            // só deve ser feito a transferencia se o mesmo tiver um saldo suficiente
+            if(Saldo >= valorTransferencia){
+                contaDestino.Depositar(valorTransferencia); // estou utilizando o metodo depositar para depositar o valor "valorTransferencia" na outra conta "ContaDestino"
+            }
+
+        }
+
+        public decimal ConsultarSaldoAtual(){
+            return Saldo;
+        }
+
+
+        
+
+
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using ExercicioBancario;
+
+namespace Course{
+    class Program
+    {
+        static void Main(string[] args){
+
+            // Instanciando os novos objetos
+
+            ContaBancaria conta1 = new ContaBancaria();
+            ContaBancaria conta2 = new ContaBancaria();
+            Random randomicoConta = new Random();
+            Random randomicoSaldo = new Random();
+
+            // Definindo dados CONTA1
+
+            Console.WriteLine("Vamos Cadastrar uma nova conta sua? ");
+
+            conta1.Numero = randomicoConta.Next(100); // definindo um numero randomico para a conta do user de até no maximo 100
+            Console.WriteLine($"O numero da sua conta é {conta1.Numero}");
+            Console.Write("Digite o seu nome de titular que deseja: ");
+            conta1.Titular = Console.ReadLine();
+            conta1.Saldo = randomicoSaldo.Next(2000); // Definindo um numero randomico para o saldo do user até 2000 mil 
+            Console.WriteLine($"O seu Saldo atual é de {conta1.Saldo}\n");
+
+            // Definindo dados CONTA2
+
+            Console.WriteLine("Vamos Cadastrar sua segunda Conta? ");
+
+            conta1.Numero = randomicoConta.Next(100); // definindo um numero randomico para a conta do user de até no maximo 100
+            Console.WriteLine($"O numero da sua segunda conta é {conta1.Numero}");
+            Console.Write("Digite o seu nome de titular que deseja: ");
+            conta2.Titular = Console.ReadLine();
+            conta2.Saldo = randomicoSaldo.Next(2000); // Definindo um numero randomico para o saldo do user até 2000 mil 
+            Console.WriteLine($"O seu Saldo atual da sua segunda conta é de {conta1.Saldo}\n");
+
+            // partindo para as operações
+
+            Console.WriteLine("Vamos agora para as operações: ");
+            bool condicao = true;
+
+            while(condicao == true){
+                // Menu de operações 
+
+                Console.WriteLine(Environment.NewLine + "Digite qual opção deseja fazer em sua conta!!");
+                Console.WriteLine("1 - Sacar.");
+                Console.WriteLine("2 - Depositar.");
+                Console.WriteLine("3 - Tranferir.");
+                Console.Write("4 - Sair do Programa.\n");
+                int opcao = Convert.ToInt32(Console.ReadLine());
+
+                if(opcao == 1){
+                    Console.WriteLine(Environment.NewLine + "Digite o valor que deseja sacar");
+                    decimal saque = Convert.ToDecimal(Console.ReadLine());
+                    conta1.Sacar(saque);
+                    Console.WriteLine($"\nApós esse saque seu saudo agora é de {conta1.ConsultarSaldoAtual()}");
+                } else if (opcao == 2){
+                    Console.WriteLine(Environment.NewLine + "Digite o valor que deseja Depositar");
+                    decimal deposito = Convert.ToDecimal(Console.ReadLine());
+                    conta1.Depositar(deposito);
+                    Console.WriteLine(Environment.NewLine + $"Após esse deposito seu saldo atua é {conta1.ConsultarSaldoAtual()}");
+                    conta1.ConsultarSaldoAtual();
+                }else if (opcao == 3){
+                    Console.WriteLine(Environment.NewLine + "Digite o valor que deseja Transferir e o nome da conta");
+                    decimal transferencia = Convert.ToDecimal(Console.ReadLine());
+                    conta1.Transferencia(conta2, transferencia);
+                    Console.WriteLine(Environment.NewLine + $"Após essa tranferencia o saldo da sua Conta 1 é de {conta1.ConsultarSaldoAtual()}");
+                    Console.WriteLine(Environment.NewLine + $"Sendo assim a sua Conta 2 após a transferencia tem o saldo de {conta2.ConsultarSaldoAtual()}");
+                } else {
+                    condicao = false;
+                }
+            }
+
+
+        
+        }
+    }
+}
